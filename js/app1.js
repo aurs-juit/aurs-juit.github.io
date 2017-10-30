60c2bd4daeedb3cacb3085b62e55f7524d5d716c
Aditya Tiwari <aditya.tiwari378@gmail.com>
2017-10-30 20:42:40 +0530

    Update app1.js

--- js/app1.js
+++ js/app1.js
@@ -22,8 +22,8 @@ particlesJS('particles-js', {
       },
       "image": {
         "src": "img/github.svg",
-        "width": 100,
-        "height": 100
+        "width": 95,
+        "height": 95
       }
     },
     "opacity": {
